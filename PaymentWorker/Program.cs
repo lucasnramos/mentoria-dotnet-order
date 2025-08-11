8a74cedb0b5c05e96c2a52ff7afceb5414fdd406
Lucas Ramos <lucasnramos@gmail.com>
2025-08-11 09:08:05 -0300

    add OrderMessage class and update Worker to process OrderMessage from RabbitMQ

--- PaymentWorker/Program.cs
+++ PaymentWorker/Program.cs
@@ -7,7 +7,13 @@ builder.Services.AddHostedService<Worker>();
 builder.Services.AddSingleton<IEventBus>(provider => new EventBus(builder.Configuration.GetSection("RabbitMq:Connection").Value,
                                                         builder.Configuration.GetSection("RabbitMq:ExchangeName").Value,
                                                         "direct"));
-
+builder.Services.AddSingleton((provider) =>
+{
+    var consumer = new Consumer((EventBus)provider.GetService<IEventBus>()!);
+    consumer.Subscribe<OrderMessage>(builder.Configuration.GetSection("RabbitMq:QueuePayment").Value,
+                                            builder.Configuration.GetSection("RabbitMq:RoutingKey").Value);
+    return consumer;
+});
 
 var host = builder.Build();
 host.Run();
